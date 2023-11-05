25a0db671e5e29e934417451af1c157a07558d5d
MatveySTEP <matrasser@mail.ru>
2023-11-05 16:01:49 +0400

    Based function

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,10 +1,15 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/MatveySTEP/URL_Shortener/store"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateShortUrl(c *gin.Context) {
 	//заглушка
 }
 func HandleShortUrlRedirect(c *gin.Context) {
-	//заглушка
+	shortUrl := c.Param("shortUrl")
+	initialUrl := store.RetrieveInitialUrl(shortUrl)
+	c.Redirect(302, initialUrl)
 }
